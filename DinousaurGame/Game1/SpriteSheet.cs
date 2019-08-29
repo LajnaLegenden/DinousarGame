afb2b7a2291d0fdb497f8a74df274b2a36c51f0e
Linus Jansson <00Jansson@gmail.com>
2019-08-29 16:58:12 +0200

    Update

---
+++ DinousaurGame/Game1/SpriteSheet.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+namespace Game1
+{
+    class SpriteSheet
+    {
+        Texture2D spriteSheet;
+
+        //Public INFO via Properties
+        private int width;
+        private int height;
+
+        //Properties
+        public int Width
+        {
+            get;
+        }
+        public int Height
+        {
+            get;
+        }
+    public SpriteSheet(string sheet)
+        {
+            spriteSheet = Game1.MyContent.Load<Texture2D>(sheet);
+            width = spriteSheet.Width;
+            height = spriteSheet.Height;
+        }
+
+        public void DefineObject(int startX, int startY, int height, int width)
+        {
+            
+        }
+
+        
+    }
+}
---
+++ DinousaurGame/Game1/spriteSheetObject.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    class spriteSheetObject
+    {
+        Texture2D texture;
+        GraphicsDevice newGraphicsDevice = new GraphicsDevice(GraphicsAdapter.DefaultAdapter, GraphicsProfile.HiDef, new PresentationParameters());
+        private int x, y, w, h;
+
+
+
+        public spriteSheetObject()
+        {
+
+        }
+
+        public Rectangle GetSourceRect()
+        {
+            return new Rectangle(x,y,w,h);
+        }
+        public void Draw()
+        {
+            Game1.SpriteBatch.Draw();
+        }
+
+    }
+}
